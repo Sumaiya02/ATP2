72685ac57512bed619d5d2f97fb5420b31e97c15
Asus <Asus@DESKTOP-TKBAVN1>
2018-02-03 09:38:31 +0600

    Initialize

---
+++ atp2sln/atp2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="atp2.MvcApplication" Language="C#" %>
---
+++ atp2sln/atp2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(atp2.Startup))]
+namespace atp2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
